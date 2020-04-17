2e6254a92ee51d035fed4278752fc9e8c6efd82e
Fernando Veronez <fernandoveronez_@hotmail.com>
2020-04-16 22:43:11 -0300

    added to lower case to email save and login

--- api/user.js
+++ api/user.js
@@ -10,7 +10,7 @@ module.exports = app =>{
     const save = (req,res) =>{
         getHash(req.body.password, hash=>{
             const password = hash
-            app.db('users').insert({name:req.body.name, email:req.body.email, password})
+            app.db('users').insert({name:req.body.name, email:req.body.email.toLowerCase(), password})
             .then(_ => res.status(204).send())
             .catch(err => res.status(500).json(err))
         })
